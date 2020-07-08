3154c5fa0c9c58035b0b9c09a8cfba838c4e1307
nishantagrawal009 <62321044+nishantagrawal009@users.noreply.github.com>
2020-07-08 12:42:04 +0530

    Profefe final (#1)
    * fii=xing
    
    * adding own agent dependencies
    
    * running the agent
    
    * fixed agent not starting issue
    
    * removing the profefe dependency
    
    * adding git ignore
    
    * changes

---
+++ agent/agentUtil.go
@@ -0,0 +1,11 @@
+package agent
+
+import "context"
+
+func StartAgent() {
+
+	agent := New("http://localhost:8081","collector")
+
+	agent.Start(context.Background())
+
+}
--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
+	"kube/agent"
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-
 )
 
 
 func main() {
 
-	go startAgent()
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w,"/welcome")
 	})
 
+	go agent.StartAgent()
+	simulate()
 	log.Println("running!")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
